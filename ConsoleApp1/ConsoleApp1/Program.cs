d6c0fbc38d83e93b891f8a04a9abee4e5cd37c83
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-19 11:21:55 +0530

    Commiting UC6 files

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,13 +8,16 @@ namespace ConsoleApp1
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HOURS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int empWage = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHours = 0;
             int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHours<=MAX_HOURS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -30,9 +33,9 @@ namespace ConsoleApp1
                         break;
 
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("emp wage is " + empWage);
-                totalEmpWage += empWage;
+                totalEmpHours += empHrs;
+
+                totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
             }
             Console.WriteLine("total employee wage is" + totalEmpWage);
         }
